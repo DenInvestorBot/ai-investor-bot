a3fb04edb5d06ee10f8a8642d92d2b3e5be1f149
DenInvestorBot <harlass85@gmail.com>
2025-07-23 15:01:21 +0300

    Update crypto_monitor.py

--- crypto_monitor.py
+++ crypto_monitor.py
@@ -2,7 +2,7 @@ import requests
 import openai
 from telegram import Bot
 
-BOT_TOKEN = "7913819667:AAGf0vL8sX7zRnwozm7AFhEyHVN0plMOjus"  # Подставь свой токен
+BOT_TOKEN = ""  # Подставь свой токен
 CHAT_ID = 1634571706  # Твой chat_id
 
 bot = Bot(token=BOT_TOKEN)
